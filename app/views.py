8bbd12f9388f1913cb28f5cd217678ef01459c6d
JoyWambui <JoyWambui@github.com>
2021-11-01 12:46:07 +0300

    Update root view to include fetched results from API call.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import views
+from .requests import get_sources
 
 @app.route("/")
 def index():
    """ View root page function that returns the index page and its data"""
-   return render_template("index.html")
+   news_sources = get_sources()
+   title = "Home of all your News Needs"
+   return render_template("index.html",title=title,sources=news_sources)
