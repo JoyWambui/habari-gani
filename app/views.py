4f1d6afdc4b0f722491867ec363e752cf20c9ad9
JoyWambui <JoyWambui@github.com>
2021-11-01 11:24:36 +0300

    Create  basic structure of app

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from app import views
+
+@app.route("/")
+def index():
+   """ View root page function that returns the index page and its data"""
+   return render_template("index.html")
