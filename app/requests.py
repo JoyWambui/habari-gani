887131e1bf6a49ed64fd9b593026b688bc7b22bf
JoyWambui <JoyWambui@github.com>
2021-11-01 12:41:50 +0300

    Create API request to fetch source objects.

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,40 @@
 from app import app
+from .models import source
+Source = source.Source
 
 api_key = app.config["NEWS_API_KEY"]
+base_url = app.config["NEWS_BASE_URL"]
+
+def get_sources():
+    """Function that gets the json response to our url request"""
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['results']:
+            sources_results_list = get_sources_response['results']
+            sources_results = process_results(sources_results_list)
+
+
+    return sources_results
+
+def process_results(sources_list):
+    """Function that processes the sources results and transforms them to a list of Source Objects"""
+    
+    sources_results = []
+    for single_source in sources_list:
+        id = single_source.get("id")
+        name = single_source.get("name")
+        description = single_source.get("description")
+        url = single_source.get("url")
+        category = single_source.get("category")
+        country = single_source.get("country")
+        
+        source_object = Source(id,name,description,url,category,country)
+        sources_results.append(source_object)
+
+    return sources_results
--- app/source_test.py
+++ app/source_test.py
@@ -7,7 +7,7 @@ class SourceTest(unittest.TestCase):
 
     def setUp(self):
         """Set up method that will run before every Test"""
-        self.new_source = Source(1, "BBC","Home of objective news","https://www.bbc.com/","General","UK")
+        self.new_source = Source("bbc", "BBC","Home of objective news","https://www.bbc.com/","General","UK")
 
     def test_instance(self):
         """Test that confirms a source object is an instance of the Source class"""
@@ -15,7 +15,7 @@ class SourceTest(unittest.TestCase):
         
     def test_correct_instantiation(self):
         """Test that confirms a source object instantiates with the right attributes."""
-        self.assertEqual(self.new_source.id,1 )
+        self.assertEqual(self.new_source.id,"bbc" )
         self.assertEqual(self.new_source.name,"BBC")
         self.assertEqual(self.new_source.description, "Home of objective news")
         self.assertEqual(self.new_source.url,"https://www.bbc.com/")
